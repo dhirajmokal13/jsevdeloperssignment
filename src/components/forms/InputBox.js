41abe0245bc8bf90b5f4d093b575d0c6f04fe9fa
Dhiraj Mokal <dhirajmokal13@gmail.com>
2024-02-13 09:18:48 +0530

    Third commit

--- src/components/forms/InputBox.js
+++ src/components/forms/InputBox.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './inputBox.css'
 
-export const InputBox = ({ type, placeholder, title, value, onChange, required }) => {
+export const InputBox = ({ type, placeholder, title, value, onChange, requiredIndication }) => {
     return (
-        <div className='inputContainer' style={{width: '91%'}}>
-            <label className="inputLabel">{title} {required && <sup style={{ color: 'red' }}>*</sup>}</label>
+        <div className='inputContainer' style={{ width: '91%' }}>
+            <label className="inputLabel">{title}{requiredIndication && <sup style={{ color: 'red' }}>*</sup>}</label>
             <input type={type} className="inputField" placeholder={placeholder} value={value} onChange={onChange} />
         </div>
     )
--- src/pages/Registartion.js
+++ src/pages/Registartion.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { InputBox } from '../components/forms/InputBox'
 import '../css/Registration.css'
 
@@ -10,20 +10,49 @@ export const Registartion = () => {
     password: '',
     company: ''
   });
-  
+
+  const formDataHandle = (key, value) => {
+    setFormData({
+      ...formData,
+      [key]: value
+    })
+  }
+
+  const Container = ({ children }) => {
+    return <div style={{ marginBottom: '3.3vh' }}>{children}</div>
+  }
+
   return (
     <div className='registrationContainer'>
       <span className='registrationTitle'>Create your PopX account</span>
-      <InputBox
-        type="email"
-        placeholder="Enter email address"
-        title="Email Address"
-        value={}
-        onChange={
-          (data) =>
-            formDataHandle("email", data.target.value)
-        }
-      />
+
+      <Container>
+        <InputBox
+          type="text"
+          placeholder="Enter Full Name"
+          title="Full Name"
+          value={formData.name}
+          requiredIndication={true}
+          onChange={
+            (data) =>
+              formDataHandle("name", data.target.value)
+          }
+        />
+      </Container>
+
+      <Container>
+        <InputBox
+          type="tel"
+          placeholder="Enter Phone Number"
+          title="Phone number"
+          value={formData.phone}
+          requiredIndication={true}
+          onChange={
+            (data) =>
+              formDataHandle("phone", data.target.value)
+          }
+        />
+      </Container>
     </div>
   )
 }
